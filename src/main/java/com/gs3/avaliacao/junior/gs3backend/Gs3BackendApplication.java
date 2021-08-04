5c3d861680ced250b7c33ff45aa77db9ce1d8c9e
Gustavo Gomes <gustavo.gomes@fornecedores.sicoob.com.br>
2021-08-04 17:38:49 -0300

    Criando o projeto

---
+++ src/main/java/com/gs3/avaliacao/junior/gs3backend/Gs3BackendApplication.java
@@ -0,0 +1,13 @@
+package com.gs3.avaliacao.junior.gs3backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gs3BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gs3BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gs3/avaliacao/junior/gs3backend/Gs3BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs3.avaliacao.junior.gs3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gs3BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
